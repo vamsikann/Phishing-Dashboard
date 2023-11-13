cd1523736dc5328219f6ccac281c6cbdb0cf227b
17863 <darianperez928@gmail.com>
2023-11-13 16:00:04 -0500

    cleaned it up

--- add_entries.py
+++ add_entries.py
@@ -3,14 +3,12 @@ import random
 import datetime
 from faker import Faker
 
-# Database path
 db_path = 'quiz_data.db'
 
 fake = Faker()
 
 
 def generate_fiu_email():
-    """Generates a random FIU email address using Faker."""
     first_name = fake.first_name().lower()
     last_name = fake.last_name().lower()
     number = random.randint(10, 999)
@@ -19,7 +17,6 @@ def generate_fiu_email():
 
 
 def generate_submit_date(start_date, end_date):
-    """Generates a random submission date within a specified range."""
     time_format = "%m/%d/%Y %I:%M:%S %p"
     start = datetime.datetime.strptime(start_date, time_format)
     end = datetime.datetime.strptime(end_date, time_format)
@@ -28,7 +25,6 @@ def generate_submit_date(start_date, end_date):
 
 
 def calculate_score(user_responses, correct_answers):
-    """Calculates the score based on user responses and correct answers."""
     score = 0
     for question, answer in user_responses.items():
         if correct_answers.get(question) == answer:
@@ -37,7 +33,6 @@ def calculate_score(user_responses, correct_answers):
 
 
 def insert_quiz_response(db_path, responder, submit_date, responses, score):
-    """Inserts a quiz response into the database."""
     conn = sqlite3.connect(db_path)
     cursor = conn.cursor()
     data = (responder, submit_date, *responses, score)
@@ -54,7 +49,6 @@ def insert_quiz_response(db_path, responder, submit_date, responses, score):
         conn.close()
 
 
-# Fetch the correct answers from the database
 def fetch_correct_answers():
     conn = sqlite3.connect(db_path)
     cursor = conn.cursor()
@@ -63,13 +57,11 @@ def fetch_correct_answers():
     return {row[0]: row[1] for row in correct_answers}
 
 
-# Generate and insert 300 entries
 correct_answers = fetch_correct_answers()
 for _ in range(300):
     email = generate_fiu_email()
     date = generate_submit_date("10/10/2023 12:00:00 AM", "11/17/2023 11:59:59 PM")
 
-    # Randomly generate user responses with 70% probability of being correct
     user_responses = {f'question{i + 1}': random.choices([correct_answers[f'question{i + 1}'], 'No'], weights=[70, 30])[0]
                       for i in range(10)}
 
--- csv_processing.py
+++ csv_processing.py
@@ -2,21 +2,17 @@ import sqlite3
 import csv
 import os
 
-# Database path
 database_path = 'quiz_data.db'
 
-# Output directory for CSV files
 output_directory = os.path.join(os.getcwd(), 'User_Data')
 
 conn = sqlite3.connect(database_path)
 cursor = conn.cursor()
 
-# Query to retrieve all entries
 query = "SELECT responder, submitDate, question1, question2, question3, question4, question5, question6, question7, question8, question9, question10 FROM quiz_responses;"
 cursor.execute(query)
 entries = cursor.fetchall()
 
-# Headers for the CSV file
 headers = ["responder", "submitDate", "r0f5036a8c67949be981582ee5359171f", "r1ce3bb4c82b7413a9518760318931831",
            "ra8c6feed45774005a4d108977d014fe7", "rabc89e36fb0d4b45a14898254f6b9e6c",
            "re7cdca2dcdf14530b24db50a3147dec5", "r06e82d4c8dd740458a2c9fb110b8cd81",
